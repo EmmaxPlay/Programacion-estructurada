e8920aa91669e94bcd4f37ff7c588df8c3719557
Emmanuel Hernandez <86961961+EmmaxPlay@users.noreply.github.com>
2022-08-10 18:29:34 -0500

    Initial commit

---
+++ 1 Principios del lenguaje c/1.2 BackEnd y FrontEnd.c
@@ -0,0 +1,12 @@
+// FrontEnd
+// se enfoca en el usuario,,  en todo con lo que podemos interactuar y lo que vemos mientras navegamos
+// nestra web busca causar una buena impresion y agradar al usuario, por lo cual usa HTML, CSS y JS
+
+// BackEnd
+// se enfoca en el servidor, en todo lo que el servidor puede hacer, por lo cual usa PHP, SQL y JS
+
+// FrontEnd y BackEnd
+// se enfoca en el usuario y el servidor, en todo lo que podemos interactuar y lo que vemos mientras navegamos
+// nestra web busca causar una buena impresion y agradar al usuario, por lo cual usa HTML, CSS y JS
+// tambien se usa PHP, SQL y JS
+
---
+++ 1 Principios del lenguaje c/1.3 Tipos de errores.c
@@ -0,0 +1,11 @@
+// Errores de compilacion
+// Se producen normalmente por el uso incorrecto de las reglas del lenguaje de programacion y suelen ser errores de sintaxos. Si existe un error de sintaxis, la computadora no puede comprender la instruccion, no se obtendra el programa t el compilador imprimira una lsita de todos los errores encontrados durante la compilacion.
+//Errores de ejecucion
+//
+
+//Errores logicos
+//Se producen en la logica del programa y la fuente del error suele ser el diseño del algoritmo
+
+//Reglas
+//Su diseño es desendente, su diseño es modular
+//La programacion estructurada sigunifica escribir un programa de acuerdo a las siguietes reglas
